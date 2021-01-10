cf73a20ad59a02f5611e1f14465cd4e6a109d23e
trebuhcsogait <schubert.tiago@gmail.com>
2021-01-10 15:09:17 -0700

    atualizando index

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 const links = [
-    {
-      label: "Week1 notes",
+  {
+      label: "Week 01:",
       url: "week1/index.html"
-    }
-  ]
+  },
+  {
+      label: "Week 02:",
+      url: "#"
+  }
+];
+
+var ol = document.querySelector("ol");
+
+for (var i of links){
+  var li = document.createElement("li");
+  var urlLink = document.createElement("a");
+  urlLink.setAttribute("href", i.url);
+  urlLink.textContent = i.label;
+  li.appendChild(urlLink);
+  ol.appendChild(li);
+}
