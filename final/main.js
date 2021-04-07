fdb7b8e1a825713fb3e74dea800ebe0b2236b10e
trebuhcsogait <schubert.tiago@gmail.com>
2021-04-07 02:40:20 -0600

    final

---
+++ final/main.js
@@ -0,0 +1,63 @@
+
+ const api_url1 = 'https://fishbase.ropensci.org//species?Fbname=';
+
+ 
+const form = document.getElementById('form')
+const search = document.getElementById('search');
+const main = document.getElementById('main');
+let show = false;
+async function getFish(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+    const dataResult = data.data;
+    let showMain = '';
+    console.log(data)
+    
+    if(dataResult==null){showMain ='<h3> Fish not found!</h3>'
+            main.innerHTML = showMain;}
+
+    dataResult.forEach( data => {
+        if(show == true){
+            showMain += `
+                    <div class="fish">
+                            <img src="${data.image}" alt="${data.FBname}">
+                            <div class="fish-info">
+                                <h3>${data.FBname}</h3>
+                                
+                            
+                                Genus: ${data.Genus}<br>
+                              
+                                ${data.Comments}
+                                </div>
+                                <p class="footer">Data taken from FishBase </p>
+                            </div>
+                            
+      
+  `;
+                            show =false;
+            main.innerHTML = showMain;}
+            
+    });
+    
+        
+    
+
+
+}
+
+getFish(api_url1);
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    if( search.value && search.value !== ''){
+        getFish(api_url1 + search.value);
+        search.value = '' 
+        show = true
+    }else{
+        
+        window.location.reload();
+    }
+
+})
+  
+//test
